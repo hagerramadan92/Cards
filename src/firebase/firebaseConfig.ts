497307fae6af8958978dec9d743d6d57c55beb07
hagerramadan92 <hagerramadan@renix4tech.com>
2026-01-15 10:44:04 +0200

    auth_

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -3,14 +3,15 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-   apiKey: "AIzaSyDofBQX5zsScMOpe0lX_mFkg6EWo8We4JA",
-  authDomain: "ecommerce-9161a.firebaseapp.com",
-  projectId: "ecommerce-9161a",
-  storageBucket: "ecommerce-9161a.firebasestorage.app",
-  messagingSenderId: "1034296304853",
-  appId: "1:1034296304853:web:8803ee7201d740ae092e5c",
-  measurementId: "G-X47MBXE9QZ"
+  apiKey: "AIzaSyAeQNbYK4HGZU3OUAEb8O7ciAn67s2Siqo",
+  authDomain: "cards-728c1.firebaseapp.com",
+  projectId: "cards-728c1",
+  storageBucket: "cards-728c1.firebasestorage.app",
+  messagingSenderId: "228725862015",
+  appId: "1:228725862015:web:56a76cd644399f9a73994a",
+  measurementId: "G-325Y2PG15D"
 };
+
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 const auth = getAuth(app);
