ec2bc7c67c2793e763f8eca4af6d9b509a0584a6
hagerramadan92 <hagerramadan@renix4tech.com>
2026-01-15 17:14:30 +0200

    _

--- components/LoginEmail/LoginWithEmail.tsx
+++ components/LoginEmail/LoginWithEmail.tsx
@@ -4,7 +4,7 @@ import { FaEnvelope } from "react-icons/fa";
 const LoginWithEmail = () => {
     return (
         <div className="flex items-center gap-2 justify-center border border-slate-200 py-3 hover:shadow-sm px-2 rounded-2xl">
-            <h1 className="text-sm font-semibold text-slate-700"> عن طريق البريد الإلكتروني</h1>
+            <h1 className="text-sm font-semibold text-slate-700">  البريد الإلكتروني</h1>
             <FaEnvelope className="text-xl text-pro-max" />
 
         </div>
